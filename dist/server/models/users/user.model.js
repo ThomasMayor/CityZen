5aed790d6b3bc0ed4bb78b65087beb79006f4495
Thomas Mayor <thomas.mayor.ge@gmail.com>
2017-08-03 21:57:19 +0200

    rm .temp folder

--- dist/server/models/users/user.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var mongoose = require("mongoose");
-// Import Schemas
-var user_schema_1 = require("./user.schema");
-// Define & export Mongoose Model with Interface
-exports.User = mongoose.model('users', user_schema_1.UserSchema);
