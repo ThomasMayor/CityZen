59c8f7c13f6aa4c7ef90b451e52ab592789dd1cf
Thomas Mayor <thomas.mayor.ge@gmail.com>
2017-08-03 21:05:10 +0200

    rm .temp folder

--- dist/server/models/reports/report.model.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var mongoose = require("mongoose");
-// Import Schemas
-var report_schema_1 = require("./report.schema");
-// Define & export Mongoose Model with Interface
-exports.Report = mongoose.model('reports', report_schema_1.ReportSchema);
