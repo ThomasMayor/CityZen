7d749ee67aeeb69dae63f27b9a187ab3acbc0425
Ateliers Nomades <info@iMac-de-Ateliers.local>
2017-08-03 13:16:28 +0200

    upd version - `date`

---
+++ dist/environments/production.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.prodVariables = {
+    environmentName: 'Production Environment',
+    ionicEnvName: 'prod',
+    // Front-end
+    apiEndpoint: 'https://cityzen-ge.herokuapp.com',
+    // Back-end
+    // mongodb://<dbuser>:<dbpassword>@ds047524.mlab.com:47524/cityzen
+    dbHost: 'mongodb://cityzenuser:cityzenuser123@ds047524.mlab.com:47524',
+    dbName: 'cityzen'
+};
---
+++ dist/server/models/users/user.schema.js
@@ -0,0 +1,58 @@
+"use strict";
+exports.__esModule = true;
+var mongoose = require("mongoose");
+exports.UserSchema = new mongoose.Schema({
+    password: {
+        type: String,
+        required: true,
+        match: /(?=.*[a-zA-Z])(?=.*[0-9]+).*/,
+        minlength: 6
+    },
+    email: {
+        type: String,
+        require: true,
+        match: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+    },
+    name: {
+        type: String,
+        require: true,
+        minlength: 3,
+        maxlength: 30
+    },
+    admin: Boolean,
+    created: {
+        type: Date,
+        required: true,
+        "default": new Date()
+    },
+    verified: Boolean,
+    profilePicture: String,
+    approvals: {
+        type: Number,
+        required: true,
+        "default": 0
+    },
+    disapprovals: {
+        type: Number,
+        required: true,
+        "default": 0
+    },
+    reports: {
+        type: Number,
+        required: true,
+        "default": 0
+    },
+    lastReport: {
+        type: Date,
+        required: true,
+        "default": new Date(2000, 0, 1)
+    }
+}, {
+    toJSON: {
+        transform: function (doc, ret, options) {
+            delete ret.password;
+            delete ret.created;
+            return ret;
+        }
+    }
+});
